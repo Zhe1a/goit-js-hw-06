b63005a34561b036cc15c619b1651b12e74d373d
Zhe1a <mgr58006@gmail.com>
2022-09-20 19:23:21 +0300

    ++

--- js/task-04.js
+++ js/task-04.js
@@ -1,18 +1,1 @@
 'use strict';
-const decrement = document.querySelector("[data-action=decrement]");
-const incremen = document.querySelector("[data-action=increment]");
-
-let counterValue = 0;
-function dataAction() {
-    document.querySelector('#value').textContent = counterValue;
-};
-const one = () => {
-  counterValue += 1;
-  dataAction()
-};
-const minusOne = () => {
-  counterValue -= 1;
-  dataAction()
-};
-incremen.addEventListener('click', one);
-decrement.addEventListener('click', minusOne);
